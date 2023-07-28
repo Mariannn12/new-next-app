fcdde25232c27262518dc77be7f101fa17e0e51c
Mariannn12 <duca_mariano@yahoo.it>
2023-07-29 00:39:12 +0200

    sfs

--- pages/home/index.js
+++ pages/home/index.js
@@ -10,7 +10,9 @@ export async function getServerSideProps(context){
 
   )
 
-  const session = getSession(context)
+  const session = await getSession(context)
+
+
   //if(!session){
     //return{
       //redirect:{
