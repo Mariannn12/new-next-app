8828e9fce1f760914729ff80e4e2fa5f9a07d80e
Mariannn12 <duca_mariano@yahoo.it>
2023-07-29 19:22:32 +0200

    fix env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,13 +8,13 @@ export const authOptions = {
             clientSecret : process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    secret : "61d8db73954c305a8d4f1b177257279d"
+    secret : process.env.NEXTAUTH_SECRET
 }
 
 export default NextAuth({
 
     providers:[GoogleProvider({clientId : process.env.GOOGLE_CLIENT_ID,clientSecret : process.env.GOOGLE_CLIENT_SECRET})],
-    secret : "61d8db73954c305a8d4f1b177257279d",
+    secret : process.env.NEXTAUTH_SECRET,
     callbacks : {
         async redirect({url, baseUrl}){
             if(url.startsWith("/")) return `${baseUrl}${url}`
