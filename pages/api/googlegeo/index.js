c054689cb1a897cc1caa98a50886e9a0299aac0b
Mariannn12 <duca_mariano@yahoo.it>
2023-07-30 20:38:15 +0200

    general changes

--- pages/api/googlegeo/index.js
+++ pages/api/googlegeo/index.js
@@ -1,8 +1,28 @@
+
 export default async function handler(req,res){
+
+    const {place_id} = req.query;
+
     try{
-        res.status(200).json({key: process.env.GOOGLE_API_KEY})
+
+        const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?place_id=${place_id}&key=${process.env.NEXT_GOOGLE_API_KEY}`)
+        const data = await response.json();
+        const country = (data.results[0].address_components).filter(object => object.types[0] === "country")[0].short_name
+        const lat = data.results[0].geometry.location.lat;
+        const lng = data.results[0].geometry.location.lng;
+        const formatted_address = data.results[0].formatted_address;
+        const addresses = []
+
+        data.results[0].address_components.forEach((addr_component) => {
+            addresses.push(addr_component.long_name)
+        });
+
+        res.status(200).send({country,formatted_address,lat,lng,addresses})
 
     }catch(error){
-        res.status(501).json({message: error})
+
+        res.status(500).send({message : error})
+
     }
+
 }
--- pages/home/index.js
+++ pages/home/index.js
@@ -2,6 +2,7 @@ import {useSession, signIn, signOut, getSession} from 'next-auth/react';
 import ResponsiveAppBar from '@/src/Components/NavBar';
 import {getServerSession} from "next-auth/next"
 import  {authOptions}  from '../api/auth/[...nextauth]';
+import { useColorScheme } from '@mui/material';
 
 export async function getServerSideProps(context){
 
@@ -11,7 +12,8 @@ export async function getServerSideProps(context){
       props:{
         usersession : session,
         userlocations : session ? await (await fetch(`https://${context.req.headers.host}/api/mongo/getuser?email=${session.user.email}`)).json() : {},
-        hostname: context.req.headers.host
+        hostname: context.req.headers.host,
+        googlekey : session ? await (await fetch(`https://${context.req.headers.host}/api/googleapikey`)).json() : {}
 
       }
     }
@@ -19,27 +21,62 @@ export async function getServerSideProps(context){
 }
 
 
-export default function SearchPlaces({usersession, userlocations,hostname}){
+function loadScript(src, position, id){
+
+  if(!position){
+    return;
+  }
+  
+  const script = document.createElement('script');
+  script.setAttribute('async', '');
+  script.setAttribute('id', id);
+  script.src = src;
+  position.appendChild(script);
+  
+}
+
+
+export default function SearchPlaces({usersession, userlocations,hostname,googlekey}){
   const {data:session} = useSession({required:true})
+
+  const loaded = React.useRef(false);
+
+  if(typeof window !== 'undefined' && !loaded.current){
+    if(!document.querySelector('#google-maps')){
+      loadScript(`https://maps.googleapis.com/maps/api/js?key=${googlekey.key}&libraries=places`,
+      document.querySelector('head'),
+      'google-maps',
+      )
+    }
+    loaded.current = true
+  }
+
+  React.useEffect(()=>{
+    ;(async function(){
+      await fetch(`https://${hostname}/api/mongo/postuser`, {
+        method : "POST",
+        headers : {
+          "Content-Type" : "application/json"
+        },
+
+        body : JSON.stringify({
+          name: useSession.user.name,
+          email: usersession.user.email,
+          created_at : new Date().toLocaleString(),
+          recentplaces : []
+        })
+      })
+    })()
+  }, [])
   
   return (
     <>
       <ResponsiveAppBar session={usersession} logOut={()=>signOut()} hostname={hostname} />
 
-      <button onClick={()=> signOut()}>Sign out</button>
+      
     
 
-      <pre>
-        {JSON.stringify(usersession,null,2)}
-      </pre>
-
-      <pre>
-
-        {JSON.stringify(userlocations,null,2)}
-      </pre>
-
-     
-  
+      
     </>
   )
 }
